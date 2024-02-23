157f2c27b53e753be8c96e91e6610be9d46456ca
Lestwa <108033937+Lestwa@users.noreply.github.com>
2024-02-24 03:54:35 +0400

    page 80 - 106 django book
     + redact models, add get absolute url
     + add email post form
     + redact template
     + add paginator
     + redirect general page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,19 +1,50 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    #постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        #если page_number находится вне диапазона, то выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        #если page_number не целое число, выдать первую страницу
+        posts = paginator.page(1)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request,
                   'blog/post/detail.html',
