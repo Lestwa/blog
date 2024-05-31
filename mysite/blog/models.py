6c9f17da93192951691f8406252de5fafc4e65da
Lestwa <108033937+Lestwa@users.noreply.github.com>
2024-05-31 20:15:41 +0400

    end code

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -31,6 +32,7 @@ class Post(models.Model):
 
     objects = models.Manager() #менеджер по умолчанию ко всем постам
     published = PublishedManager() #менеджер по опубликованным постам
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,6 +4,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_share(request, post_id):
@@ -30,8 +32,12 @@ def post_share(request, post_id):
                                                     'sent': sent})
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
     #постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
@@ -39,15 +45,14 @@ def post_list(request):
     try:
         posts = paginator.page(page_number)
     except EmptyPage:
-        #если page_number находится вне диапазона, то выдать последнюю страницу
         posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
-        #если page_number не целое число, выдать первую страницу
         posts = paginator.page(1)
 
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -57,15 +62,22 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-
+    #список активных комментариев к этому посту
     comments = post.comments.filter(active=True)
+    #форма для комментариев пользователей
     form = CommentForm()
+
+    #список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 @require_POST
